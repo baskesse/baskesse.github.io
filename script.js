f06c1a56e5d101349869081ec515b628deb5f033
baskesse <104189583+baskesse@users.noreply.github.com>
2022-05-13 14:35:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 function deplacement_balle(x,y) {
-	document.getElementById("balle").style.deplacement_balle
+	document.getElementById("balle").style.left=x+"px"
+	document.getElementById("balle").style.left=y+"px"
+}
+
+function deplacement_racket1(y){
+	if (y>300)
+		y=300
+	if (y<0)
+		y=0	
+	document.getElementById("racket1").style.top=y+"px"	
+}
+
+function deplacement_racket2(y){
+	if (y>300)
+		y=300
+	if (y<0)
+		y=0	
+	document.getElementById("racket2").style.top=y+"px"	
 }
