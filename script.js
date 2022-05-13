4658881b3ee8ae84755568a2d65e5750a41da136
baskesse <104189583+baskesse@users.noreply.github.com>
2022-05-13 14:39:44 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-function deplacement_balle(x,y) {
-	document.getElementById("balle").style.left=x+"px"
-	document.getElementById("balle").style.left=y+"px"
+x=650;
+y=365;
+dx=5;
+dy=5;
+function deplacement(x,y){
+document.getElementById("balle").style.left=x+"px"
+document.getElementById("balle").style.top=y+"px"
+x += dx;
+y += dy;
+console.log(x,y)
 }
+setInterval(deplacement,20)
 
 function deplacement_racket1(y){
 	if (y>300)
