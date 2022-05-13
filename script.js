616549b8b80c8abf146e9af2e8e47fad22e36da9
baskesse <104189583+baskesse@users.noreply.github.com>
2022-05-13 14:57:31 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ x=650;
 y=365;
 dx=5;
 dy=5;
-function deplacement(x,y){
+function deplacement(){
 document.getElementById("balle").style.left=x+"px"
 document.getElementById("balle").style.top=y+"px"
 x += dx;
